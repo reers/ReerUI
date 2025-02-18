8af1ad5c645279b6e076a9ccdc27059e4b175bb7
Phoenix <x.rhythm@qq.com>
2025-02-18 15:59:50 +0800

    Use sections

--- Demo/SharedDemo/Sources/SharedDemo/Sidebar.swift
+++ Demo/SharedDemo/Sources/SharedDemo/Sidebar.swift
@@ -16,52 +16,63 @@ public struct Sidebar: View {
     
     public var body: some View {
         List {
-            NavigationLink(destination: Loading().padding()) {
-                LeftLabel(title: "Loading", icon: { Loading(gradientColor: .primary, ballColor: .primary) })
-            }
-            NavigationLink(destination: ProgressRingDemo()) {
-                LeftLabel(title: "ProgressRing", icon: {
-                    ProgressRing(progress: $progress, gradientColors: [.cyan, .blue], lineWidthRatio: 0.3)
-                        .frame(width: 15, height: 15)
-                })
-            }
-            NavigationLink(destination: GradientProgressRingDemo()) {
-                LeftLabel(title: "GradientProgressRing", icon: {
-                    GradientProgressRing(
-                        angleLength: 240,
-                        rotateAngle: 150,
-                        progress: $progress,
-                        strokeWidth: 4,
-                        bgColor: Color(.lightGray.withAlphaComponent(0.5)),
-                        gradientStops: [
-                            .init(color: Color(.re(hex: 0xFF86AA)), location: 0.17),
-                            .init(color: Color(.re(hex: 0xFF4B75)), location: 0.34),
-                            .init(color: Color(.re(hex: 0xEF3CFF)), location: 0.5),
-                            .init(color: Color(.re(hex: 0x7E5EFF)), location: 0.66),
-                            .init(color: Color(.re(hex: 0x8064FF)), location: 0.83),
-                        ]
-                    )
-                })
-            }
-            NavigationLink(destination: StunningEffect().padding()) {
-                LeftLabel(title: "StunningEffect", icon: { Circle() })
-            }
-            #if os(macOS)
-            if #available(macOS 15.0, *) {
-                NavigationLink(destination: IntelligenceLightView().padding()) {
-                    LeftLabel(title: "IntelligenceLight", icon: { RoundedRectangle(cornerRadius: 2) })
+            Section("Components Demo") {
+                NavigationLink(destination: Loading().padding()) {
+                    LeftLabel(title: "Loading", icon: { Loading(gradientColor: .primary, ballColor: .primary) })
                 }
+                
+                NavigationLink(destination: ProgressRingDemo()) {
+                    LeftLabel(title: "ProgressRing", icon: {
+                        ProgressRing(progress: $progress, gradientColors: [.cyan, .blue], lineWidthRatio: 0.3)
+                            .frame(width: 15, height: 15)
+                    })
+                }
+                
+                NavigationLink(destination: GradientProgressRingDemo()) {
+                    LeftLabel(title: "GradientProgressRing", icon: {
+                        GradientProgressRing(
+                            angleLength: 240,
+                            rotateAngle: 150,
+                            progress: $progress,
+                            strokeWidth: 4,
+                            bgColor: Color(.lightGray.withAlphaComponent(0.5)),
+                            gradientStops: [
+                                .init(color: Color(.re(hex: 0xFF86AA)), location: 0.17),
+                                .init(color: Color(.re(hex: 0xFF4B75)), location: 0.34),
+                                .init(color: Color(.re(hex: 0xEF3CFF)), location: 0.5),
+                                .init(color: Color(.re(hex: 0x7E5EFF)), location: 0.66),
+                                .init(color: Color(.re(hex: 0x8064FF)), location: 0.83),
+                            ]
+                        )
+                    })
+                }
+                
+                #if os(macOS)
+                if #available(macOS 15.0, *) {
+                    NavigationLink(destination: IntelligenceLightView().padding()) {
+                        LeftLabel(title: "IntelligenceLight", icon: { RoundedRectangle(cornerRadius: 2) })
+                    }
+                }
+                #endif
+                
+                #if os(iOS)
+                NavigationLink(destination: HamburgerMenu(isActive: $showMenu, lineLength: 80, lineHeight: 4).padding()) {
+                    LeftLabel(title: "HamburgerMenu", icon: { HamburgerMenu(isActive: $showMenu, lineLength: 20, lineHeight: 2) })
+                }
+                #endif
             }
-            #endif
             
-            #if os(iOS)
-            NavigationLink(destination: HamburgerMenu(isActive: $showMenu, lineLength: 80, lineHeight: 4).padding()) {
-                LeftLabel(title: "HamburgerMenu", icon: { HamburgerMenu(isActive: $showMenu, lineLength: 20, lineHeight: 2) })
+            Section("UI Showcase") {
+                NavigationLink(destination: StunningEffect().padding()) {
+                    LeftLabel(title: "StunningEffect", icon: { Circle() })
+                }
+                
+                #if os(iOS)
+                NavigationLink(destination: VariableBlurDemo().padding()) {
+                    LeftLabel(title: "VariableBlur", icon: { Image(systemName: "moonphase.waxing.crescent").resizable().aspectRatio(contentMode: .fit) })
+                }
+                #endif
             }
-            NavigationLink(destination: VariableBlurDemo().padding()) {
-                LeftLabel(title: "VariableBlur", icon: { Image(systemName: "moonphase.waxing.crescent").resizable().aspectRatio(contentMode: .fit) })
-            }
-            #endif
         }
         .listStyle(SidebarListStyle())
     }
